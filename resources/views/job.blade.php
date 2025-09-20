14144fe1ba0749df41c35bf3b96b06b85b99ffcc
Zyrhea16 <zyrheab@gmail.com>
2025-09-20 16:04:08 +0800

    Chapter 3 : Connecting to the Database with Eloquent

--- resources/views/job.blade.php
+++ resources/views/job.blade.php
@@ -3,8 +3,8 @@
         Job 
     </x-slot:heading> 
  
-    <h2 class="font-bold text-lg">{{ $job['title'] }}</h2> 
-    <p> 
-        This job pays {{ $job['salary'] }} per year. 
-    </p> 
+    <h2 class="font-bold text-lg">{{ $job->title }}</h2>
+    <p>
+        This job pays {{ $job->salary }} per year.
+    </p>
 </x-layout> 
--- routes/web.php
+++ routes/web.php
@@ -13,13 +13,5 @@ Route::get('/jobs/{id}', fn($id) => view('job', [
     'job' => Job::find($id)
 ]));
 
-Route::get('/jobs', function () {
-    return view('jobs', [
-        'jobs' => [
-            ['id' => 1, 'title' => 'Director', 'salary' => '$50,000'],
-            ['id' => 2, 'title' => 'Programmer', 'salary' => '$10,000'],
-            ['id' => 3, 'title' => 'Teacher', 'salary' => '$40,000'],
-        ]
-    ]);
-});
+
 
